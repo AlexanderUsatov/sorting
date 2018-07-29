adb5cd5247ff2d57a75c28b8a6d8d09a65f0833e
Igor Manushin <igor.manushin@gmail.com>
2018-07-29 14:46:45 +0300

    add draft jmh tests

---
+++ src/src/jmh/kotlin/me/usatov/alexander/sorting/benchmarks/CompareSortingTestMethods.kt
@@ -0,0 +1,56 @@
+package me.usatov.alexander.sorting.benchmarks
+
+import me.usatov.alexander.sorting.Main
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.*
+
+
+@BenchmarkMode(Mode.All)
+@Warmup(iterations = 10)
+@Measurement(iterations = 100, batchSize = 10)
+open class CompareSortingTestMethods {
+    private companion object {
+        const val randomSeed = 34855347605451L
+    }
+
+    @Benchmark
+    fun defaultSortOf1000Elements(blackhole: Blackhole, state: SortState) {
+        blackhole.consume(state.arrayToSortWith1000Elements.sort())
+    }
+
+    @Benchmark
+    fun hashMapSortOf1000Elements(blackhole: Blackhole, state: SortState) {
+        blackhole.consume(Main.usatovProkuratSortUsingHashMap(state.arrayToSortWith1000Elements))
+    }
+
+    @Benchmark
+    fun defaultSortOf100000Elements(blackhole: Blackhole, state: SortState) {
+        blackhole.consume(state.arrayToSortWith1000Elements.sort())
+    }
+
+    @Benchmark
+    fun hashMapSortOf100000Elements(blackhole: Blackhole, state: SortState) {
+        blackhole.consume(Main.usatovProkuratSortUsingHashMap(state.arrayToSortWith1000Elements))
+    }
+
+    @State(Scope.Benchmark)
+    class SortState{
+        @Setup
+        fun setUp() {
+            arrayToSortWith1000Elements = generateArray(1000)
+            arrayToSortWith100000Elements = generateArray(100_000)
+        }
+
+        var arrayToSortWith1000Elements: Array<out Int> = emptyArray()
+        var arrayToSortWith100000Elements: Array<out Int> = emptyArray()
+
+        private fun generateArray(length: Int): Array<out Int> {
+            val random = Random(randomSeed)
+
+            return (0..length).map {
+                random.nextInt()
+            }.toTypedArray()
+        }
+    }
+}
