30ae58d7225f74eda17b4c6aad0a295ab5dfdd8b
AlexanderUsatov <41695664+AlexanderUsatov@users.noreply.github.com>
2018-07-30 12:51:56 +0300

    Bug fix in usatovProkuratSortUsingHashMap

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer[] arr = generateArr(10_000_000, 0, 1_000_000);
+        Integer[] arr = generateArr(10_000_000, 0, 10_000_000);
         Integer[] a1 = arr.clone();
         Integer[] a2 = arr.clone();
         Integer[] a3 = arr.clone();
@@ -24,8 +24,20 @@ public class Main {
         System.out.println(System.currentTimeMillis() - start + " Usatov-Prokurat sort");
 
         start = System.currentTimeMillis();
-        usatovProkuratSortUsingMyHash(a3);
+        usatovProkuratSortUsingMyHashTable(a3);
         System.out.println(System.currentTimeMillis() - start + " Usatov-Prokurat sort on my Hashtable");
+    }
+
+    static void usatovProkuratSortUsingMyHashTable(Integer[] arr) {
+        MyHashTable mht = new MyHashTable();
+        for (Integer i : arr)
+            mht.add(i);
+        MyPair[] res = mht.getPairs();
+        int ptr = 0;
+        Arrays.sort(res, Comparator.comparingInt(o -> o.first));
+        for (MyPair mp : res)
+            for (int i = 0; i < mp.second; i++)
+                arr[ptr++] = mp.first;
     }
 
     static void usatovSort(Integer[] arr) {
@@ -56,22 +68,14 @@ public class Main {
             if (temp == null) hm.put(i, 1);
             else hm.put(i, temp + 1);
         }
+
+        ArrayList<Integer> keys = new ArrayList<>(hm.keySet().size());
+        keys.addAll(hm.keySet());
+        keys.sort(Comparator.naturalOrder());
         int ptr = 0;
-        for (Integer i : hm.keySet())
+        for (Integer i : keys)
             for (int j = 0; j < hm.get(i); j++)
                 arr[ptr++] = i;
-    }
-    
-    static void usatovProkuratSortUsingMyHashTable(Integer[] arr) {
-        MyHashTable mht = new MyHashTable();
-        for (Integer i : arr)
-            mht.add(i);
-        MyPair[] res = mht.getPairs();
-        int ptr = 0;
-        Arrays.sort(res, Comparator.comparingInt(o -> o.first));
-        for (MyPair mp : res)
-            for (int i = 0; i < mp.second; i++)
-                arr[ptr++] = mp.first;
     }
 
     static Integer[] generateArr(int size, int a, int b) {
@@ -79,6 +83,12 @@ public class Main {
         for (int i = 0; i < size; i++)
             arr[i] = (int) (Math.random() * (b - a + 1) + a);
         return arr;
+    }
+
+    static void printArr(Integer[] arr) {
+        for (Integer i : arr)
+            System.out.print(i + " ");
+        System.out.println();
     }
 
 }
